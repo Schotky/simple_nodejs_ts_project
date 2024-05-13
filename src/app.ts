3ee5fb399ce2bbddb7677fb3e64ca9309fe14344
Boris Kosoric <boris.kosoric@zuehlke.com>
2024-05-13 12:59:08 +0200

    fix live reload

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,7 @@
-function trap(height: number[]): number {
-  let left = 0,
-    right = height.length - 1;
-  let maxLeft = 0,
-    maxRight = 0;
-  let trappedWater = 0;
+const someFunction = (nums: number[]): number[] => {
+  let result: number[] = [...nums, 1, 3];
 
-  while (left < right) {
-    if (height[left] < height[right]) {
-      if (height[left] >= maxLeft) {
-        maxLeft = height[left];
-      } else {
-        trappedWater += maxLeft - height[left];
-      }
-      left++;
-    } else {
-      if (height[right] >= maxRight) {
-        maxRight = height[right];
-      } else {
-        trappedWater += maxRight - height[right];
-      }
-      right--;
-    }
-  }
+  return result;
+};
 
-  return trappedWater;
-}
-
-console.log(trap([4, 2, 0, 3, 2, 5]));
+console.log(someFunction([0, 1, 2, 4, 5, 7]));
